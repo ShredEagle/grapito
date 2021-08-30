145a596f4997ad3f0606ec1b0bc8a1c7d14d00c9
Adrien <Adnn@users.noreply.github.com>
2021-08-30 15:06:28 +0200

    Fix anchor sight: reset sight if selected anchor exceeds reach distance.

--- src/app/grapkimbo/Systems/ControlAnchorSight.cpp
+++ src/app/grapkimbo/Systems/ControlAnchorSight.cpp
@@ -22,16 +22,23 @@ void ControlAnchorSight::positionSight(AnchorSelector & aSelector,
                                        Position & aGeometry,
                                        math::Vec<2, double> aInputDirection) const
 {
+    Position playerGeometry = aSelector.player->get<Position>();
+
     auto positionProvider = [](const Rectangle<double> aCandidate)
     {
         return aCandidate.center();
     };
 
-    auto filter = [&](const AnchorWrap & anchor, math::Vec<2, double> aVec, double aNormSquared)
+    auto filter = [&](const AnchorWrap & anchor, math::Position<2, double> aCandidate, math::Position<2, double> aBasePosition, double aNormSquared)
     {
+        math::Vec<2, double> vec = aCandidate - aBasePosition;
         return anchor != aSelector.anchor  // eliminate the currently selected anchor
-               && aVec.getNormSquared() < std::min(aNormSquared, aSelector.reachDistanceSquared)
-               && abs(math::getOrientedAngle(aVec, aInputDirection)) < aSelector.tolerance
+               // The candidate must be within the player's reach distance
+               && (aCandidate - playerGeometry.position).getNormSquared() < aSelector.reachDistanceSquared 
+               // The candidate must be better than the previously selected candidate (i.e. closer to basePosition)
+               && vec.getNormSquared() < aNormSquared
+               // The candidate must be within the angular tolerance of the input direction
+               && abs(math::getOrientedAngle(vec, aInputDirection)) < aSelector.tolerance
             ;
     };
 
@@ -51,13 +58,21 @@ void ControlAnchorSight::positionSight(AnchorSelector & aSelector,
         aSelector.commit();
     }
 
+    // If the anchor got further away from the player than the reach distance, the anchor is reset.
+    if (aSelector.anchor
+        && (aSelector.anchor->get<Position>().position - playerGeometry.position).getNormSquared()
+           > aSelector.reachDistanceSquared)
+    {
+        aSelector.anchor = nullptr;
+    }
+
     if (auto anchor = aSelector.currentAnchor())
     {
         aGeometry = anchor->get<Position>();
     }
     else
     {
-        aGeometry = aSelector.player->get<Position>();
+        aGeometry = playerGeometry;
     }
 }
 
