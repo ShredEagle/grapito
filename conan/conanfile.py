79d035e9b80d7d7253496d90791eb567ba74beed
François Poizat <francois.poizat@gmail.com>
2021-09-29 16:09:11 +0200

    update conanfile

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -25,10 +25,11 @@ class GrapitoConan(ConanFile):
         ("glad/0.1.34"),
         ("imgui/1.84.2"),
 
-        ("aunteater/6afa38a266@adnn/develop"),
-        ("graphics/d3958e3ec1@adnn/develop"),
-        ("math/4086708c05@adnn/develop"),
-        ("websocket/84bbb84476@adnn/develop"),
+        ("aunteater/bcd4c5dc43@adnn/develop"),
+        ("graphics/15abb1adc9@adnn/develop"),
+        ("math/51c3947279@adnn/develop"),
+        ("websocket/60baf7b620@adnn/develop"),
+        
     )
 
     build_requires = ("cmake/3.20.4")
