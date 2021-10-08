499556bb424d802dca19c82e1baf90b8f4a4e74e
Adrien <Adnn@users.noreply.github.com>
2021-10-08 11:57:27 +0200

    Minor: code text spacing and alignment.

--- src/app/grapkimbo/Utils/HomogeneousTransformation.h
+++ src/app/grapkimbo/Utils/HomogeneousTransformation.h
@@ -8,6 +8,7 @@
 namespace ad {
 namespace grapito {
 
+
 inline math::Matrix<3, 3, double> createPrefixedTransform(math::Radian<double> angle, Position2 center)
 {
     double x = center.x();
@@ -15,15 +16,17 @@ inline math::Matrix<3, 3, double> createPrefixedTransform(math::Radian<double> a
     return { 
         cos(angle),                            sin(angle),                           0.,
         -sin(angle),                           cos(angle),                           0.,
-        -x * cos(angle) + y * sin(angle) + x, -x * sin(angle) - y * cos(angle) + y, 1.
+        -x * cos(angle) + y * sin(angle) + x,  -x * sin(angle) - y * cos(angle) + y, 1.
     };
 }
+
 
 inline Position2 transformPosition(Position2 position, math::Matrix<3, 3, double> transform)
 {
     math::Position<3, double> homoPos = math::homogeneous::makePosition(position) * transform;
     return {homoPos.x(), homoPos.y()};
 }
+
 
 inline Position2 transformPosition(Position2 position, math::Radian<double> angle, Position2 center)
 {
@@ -32,17 +35,19 @@ inline Position2 transformPosition(Position2 position, math::Radian<double> angl
     auto transformMatrix = math::Matrix<3, 3, double>{ 
         cos(angle),                            sin(angle),                           0.,
         -sin(angle),                           cos(angle),                           0.,
-        -x * cos(angle) + y * sin(angle) + x, -x * sin(angle) - y * cos(angle) + y, 1.
+        -x * cos(angle) + y * sin(angle) + x,  -x * sin(angle) - y * cos(angle) + y, 1.
     };
 
     return transformPosition(position, transformMatrix);
 }
+
 
 inline Vec2 transformVector(Vec2 aVector, math::Matrix<3, 3, double> transform)
 {
     math::Vec<3, double> homoPos = math::homogeneous::makeVec(aVector) * transform;
     return {homoPos.x(), homoPos.y()};
 }
+
 
 inline Vec2 transformVector(Vec2 aVector, math::Radian<double> angle)
 {
