b6bd955399b9da32ac21d35cecb5577d12ed32ab
Adrien <Adnn@users.noreply.github.com>
2022-02-04 16:18:41 +0100

    Fix: Prevent adding a player if controller is already present.

--- src/app/grapkimbo/Context/PlayerList.cpp
+++ src/app/grapkimbo/Context/PlayerList.cpp
@@ -57,12 +57,17 @@ int PlayerList::addPlayer(Controller aControllerId, PlayerJoinState aJoinState)
 {
     int availableSlot = getNextSlot();
 
-    if (availableSlot != -1)
+    if (availableSlot != -1
+        && std::ranges::find_if(mPlayerList, [&](const auto & state)
+            {
+                return state.mControllerId == aControllerId;
+            }) == mPlayerList.end())
     {
         mPlayerList.emplace_back(PlayerControllerState{availableSlot, aControllerId, aJoinState});
+        return availableSlot;
     }
 
-    return availableSlot;
+    return -1;
 }
 
 void PlayerList::removePlayer(Controller aControllerId)
