c4a4f131363f6b4bb0ee0824a42898e188214c08
Adrien <Adnn@users.noreply.github.com>
2022-02-08 08:52:42 +0100

    f

--- src/app/grapkimbo/Systems/CompetitionRule.cpp
+++ src/app/grapkimbo/Systems/CompetitionRule.cpp
@@ -389,11 +389,11 @@ CompetitionRule::CompetitionRule(aunteater::EntityManager & aEntityManager,
     mCameras{aEntityManager},
     mCameraPoints{aEntityManager},
     mControlSystem{std::move(aControlSystem)},
+    mRenderToScreenSystem{std::move(aRenderToScreenSystem)},
     mContext{std::move(aContext)},
     mPhases{setupGamePhases(mContext, *this)},
     // ATTENTION the state machine is entering the first state directly, before CompetitionRule is done cting.
-    mPhaseMachine{mPhases[ExpectPlayers]},
-    mRenderToScreenSystem{std::move(aRenderToScreenSystem)}
+    mPhaseMachine{mPhases[ExpectPlayers]}
 {}
 
 
