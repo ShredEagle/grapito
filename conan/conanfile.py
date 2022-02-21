ac5629cf76731d804ab3cb85c300d45149773b57
Adrien <Adnn@users.noreply.github.com>
2022-02-21 18:30:16 +0100

    [conan] Update graphics reference.

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -34,7 +34,7 @@ class GrapitoConan(ConanFile):
         ("vorbis/1.3.7"),
 
         ("aunteater/50a0c94674@adnn/develop"),
-        ("graphics/fd9497ef21@adnn/develop"),
+        ("graphics/787806d343@adnn/develop"),
         ("math/fd9b30cce0@adnn/develop"),
         ("sounds/ff9fabb1d0@adnn/develop"),
         ("websocket/ef5d5bf4d9@adnn/develop"),
